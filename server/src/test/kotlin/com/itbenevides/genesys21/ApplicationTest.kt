298e5512fe04142c061acab6cd4f02135b48359f
victorben <victorkoto@gmail.com>
2026-01-14 07:05:29 -0300

    ci

--- server/src/test/kotlin/com/itbenevides/genesys21/ApplicationTest.kt
+++ server/src/test/kotlin/com/itbenevides/genesys21/ApplicationTest.kt
@@ -15,6 +15,7 @@ class ApplicationTest {
         }
         val response = client.get("/")
         assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("Ktor: ${Greeting().greet()}", response.bodyAsText())
+        val expectedBody = "Ktor: ${Greeting().greet()} | Firebase Admin: Ativo"
+        assertEquals(expectedBody, response.bodyAsText())
     }
 }
