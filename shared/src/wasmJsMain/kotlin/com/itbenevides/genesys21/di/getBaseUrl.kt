e0469bae4724bb7daac555eb2485d8c76b8ddeb4
victorben <victorkoto@gmail.com>
2026-01-17 20:24:02 -0300

    fix aws

--- shared/src/wasmJsMain/kotlin/com/itbenevides/genesys21/di/getBaseUrl.kt
+++ shared/src/wasmJsMain/kotlin/com/itbenevides/genesys21/di/getBaseUrl.kt
@@ -1,3 +1,15 @@
 package com.itbenevides.genesys21.di
 
-actual fun getBaseUrl(): String = "http://localhost:8080"
+import kotlinx.browser.window
+
+actual fun getBaseUrl(): String {
+    // Detecta o IP ou Domínio atual (ex: 54.232.xx.xx) e aponta para a porta da API
+    val host = window.location.hostname
+    val protocol = window.location.protocol
+    
+    return if (host == "localhost" || host == "127.0.0.1") {
+        "$protocol//localhost:8080"
+    } else {
+        "$protocol//$host:8080"
+    }
+}
