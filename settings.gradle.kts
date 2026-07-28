58369c836ec22e3cf8bccecd4f2b57a3cc1e65ef
victorben <victorkoto@gmail.com>
2026-07-28 19:59:27 -0300

    chore: enable automatic JDK provisioning with foojay resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,9 @@ rootProject.name = "Genesys21"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
     repositories {
         google()
         mavenCentral()
