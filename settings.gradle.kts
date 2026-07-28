202dcf9717b9af64e5ca841a4ec3ac026b7f2195
victorben <victorkoto@gmail.com>
2026-07-28 20:05:01 -0300

    fix: move foojay plugin to top-level for correct toolchain resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,11 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "Genesys21"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    }
     repositories {
         google()
         mavenCentral()
